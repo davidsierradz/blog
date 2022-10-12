1fd7cc67c360d3c301777535bd5100549134e317
davidsierradz <davidsierradz@gmail.com>
2022-10-12 04:15:55 +0000

    feat: setup basic hugo site

--- flake.nix
+++ flake.nix
@@ -2,15 +2,37 @@
   description = "A basic flake with a shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.hugo-theme-etch = {
+    url = "github:LukasJoswiak/etch";
+    flake = false;
+  };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    hugo-theme-etch,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      packages.blog = pkgs.stdenv.mkDerivation {
+        name = "blog";
+        src = ./blog;
+        buildPhase = ''
+          mkdir -p themes
+          ln -s ${hugo-theme-etch} themes/etch
+          ${pkgs.hugo}/bin/hugo --minify
+        '';
+        installPhase = ''
+          cp -r public $out
+        '';
+        meta = with pkgs.lib; {
+          description = "Things I consider interesting";
+          license = licenses.mit;
+          platforms = platforms.all;
+        };
+      };
       devShell = pkgs.mkShell {
         nativeBuildInputs = [pkgs.hugo];
         buildInputs = [];
